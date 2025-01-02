3e3138571fac39e97aed67444089553bcfc7812e
Camilo Corea <camilokorea@hotmail.com>
2025-01-02 14:13:48 -0600

    - botones de edicion

--- torah-administrador/torah-administrador/src/components/Libro.jsx
+++ torah-administrador/torah-administrador/src/components/Libro.jsx
@@ -35,8 +35,28 @@ const Libro = () => {
                     (
                         <Container>
                             <Row>
-                                <h4>{libro?.nombre}</h4>
-                                <p>Abreviatura: {libro?.abreviacion}</p>
+                                <Container>
+                                    <Row className="d-flex justify-content-left align-items-left">
+                                        <Col xs={3} sm={2} md={2} lg={1}>
+                                            <Button variant="primary" size="sm">
+                                                <FaPencilAlt />
+                                            </Button>
+                                        </Col>
+                                        <Col xs={9} sm={10} md={10} lg={11}>
+                                            <h4>{libro?.nombre}</h4>
+                                        </Col>
+                                    </Row>
+                                    <Row className="d-flex justify-content-left align-items-left">
+                                        <Col xs={3} sm={2} md={2} lg={1}>
+                                            <Button variant="primary" size="sm">
+                                                <FaPencilAlt />
+                                            </Button>
+                                        </Col>
+                                        <Col xs={9} sm={10} md={10} lg={11}>
+                                            <p>Abreviatura: {libro?.abreviacion}</p>
+                                        </Col>
+                                    </Row>
+                                </Container>
                                 <h3>Capítulos</h3>
                                 <Accordion defaultActiveKey="0">
                                     {libro?.capitulos.map((capitulo, indexCapitulo) => (
@@ -47,10 +67,9 @@ const Libro = () => {
                                                     <Container key={indexVersiculo.toString()}>
                                                         <Row className="d-flex justify-content-left align-items-left">
                                                             <Col xs={3} sm={2} md={2} lg={1}>
-                                                                <Button variant="primary">
+                                                                <Button variant="primary" size="sm">
                                                                     <FaPencilAlt />
                                                                 </Button>
-
                                                             </Col>
                                                             <Col xs={9} sm={10} md={10} lg={11}>
                                                                 <p className='hover-text'>{versiculo}</p>
